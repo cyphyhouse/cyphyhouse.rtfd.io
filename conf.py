fea2a171d5350c4f79fb04bc88303b1885aaedf5
Hsieh, Chiao <chsieh16@illinois.edu>
2019-06-03 14:32:22 -0500

    Move index.rst for homepage

--- conf.py
+++ conf.py
@@ -43,7 +43,7 @@ templates_path = ['docs/_templates']
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'docs/index'
+master_doc = 'index'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
